b51e7badf0df6349f83653f81d520fa72a45c31a
PedroEtche <pedro.etchegoyen@gmail.com>
2026-03-18 08:52:14 -0300

    uso imagen mas chica para nc

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -2,11 +2,7 @@
 
 MENSAJE="Docker testing..."
 
-# Crear container que usa netcat (Cliente)
-docker build -t echo-test -f echo-test/Dockerfile . 
-# Correr cliente (netcat) y capturar respuesta 
-RESPUESTA=$(docker run --rm --name echo-test --network tp0_testing_net echo-test \
-    -c "echo $MENSAJE | nc server 12345")
+RESPUESTA=$(docker run --rm --network=tp0_testing_net alpine /bin/sh -c "echo '$MENSAJE' | nc server 12345")
 
 # Validar
 if [ "$RESPUESTA" = "$MENSAJE" ]; then
